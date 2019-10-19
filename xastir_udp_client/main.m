172e69fef2be1a841f94a65c2c0cb44741c5f493
Jeff McLeman <jeff.mcleman@mac.com>
2019-10-19 15:16:24 -0400

    Initial Commit

---
+++ xastir_udp_client/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  xastir_udp_client
+//
+//  Created by Jeff McLeman on 10/19/19.
+//  Copyright © 2019 Jeff McLeman. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
